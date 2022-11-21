213c29de5811eb6da3ee9f02983c8d29bc04aa78
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-21 17:25:33 +0100

    creazione menu statico

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+    name: 'App',
+    components: {
+        AppHeader
+    }
+}
 
 </script>
 
 <template>
-
+    <AppHeader></AppHeader>
+    <!-- /.site_header -->
+    <main id="site_main"></main>
+    <!-- /.site_main -->
+    <footer id="site_footer"></footer>
+    <!-- /.site_footer -->
 </template>
 
 <style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header id="site_header">
+        <div class="menu">
+            <div class="container d-flex justify-content-between mt-5">
+
+                <img src="../assets/img/theme_eduprime_logo.png" alt="">
+
+                <!-- /.left -->
+                <div class="right d-flex">
+                    <ul class="d-flex mt-3">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Courses</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                        <li>
+                            <a href="#">Pages</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                        <li>
+                            <a href="#">Purchase</a>
+                        </li>
+                    </ul>
+                    <div class="align-self-center ">
+                        <a class=" button text-uppercase align-self-center rounded-pill" href="#">View Courses</a>
+                    </div>
+                </div>
+
+
+
+                <!-- /.right -->
+            </div>
+            <!-- /.menu -->
+        </div>
+        <!-- /.contanier -->
+    </header>
+</template>
+
+<style lang="scss" scoped>
+img {
+    width: 20%;
+}
+
+ul {
+    list-style: none;
+
+    li {
+        margin: 0 0.50rem;
+
+        a {
+            text-decoration: none;
+            color: white;
+
+            &:hover {
+                color: #e9d758;
+            }
+        }
+    }
+
+}
+
+.button {
+    padding: 1rem;
+    background-color: #e9d758;
+    text-decoration: none;
+    color: white;
+}
+</style>
