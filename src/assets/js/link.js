23f9812c8d37bb84e6c7c53452dde25b2078a1a2
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-23 13:51:19 +0100

    Creazione componets link.js

---
+++ src/assets/js/link.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        text: `Request a website`,
+        href: `#`
+    },
+    {
+        text: `Browse Themes`,
+        href: `#`
+    },
+    {
+        text: `Payament options`,
+        href: `#`
+    },
+    {
+        text: `Support System`,
+        href: `#`
+    },
+    {
+        text: `Checkout`,
+        href: `#`
+    },
+    {
+        text: `Purchase Theme`,
+        href: `#`
+    }
+]
