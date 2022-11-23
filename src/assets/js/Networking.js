b5c1d680343d58f488b5f965a27035312716ed29
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-23 14:50:46 +0100

    Creazione networking

---
+++ src/assets/js/Networking.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        text: `Purchase Theme`,
+        href: `#`
+    },
+    {
+        text: `Our Benefits`,
+        href: `#`
+    },
+    {
+        text: `Out Team`,
+        href: `#`
+    },
+    {
+        text: `Our Service`,
+        href: `#`
+    },
+    {
+        text: `Other Products`,
+        href: `#`
+    },
+    {
+        text: `My account`,
+        href: `#`
+    }
+
+]
+
